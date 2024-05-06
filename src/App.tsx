81badd639dc23491130f9be281a6745bfe330948
Temirlan Omarov <omarovtemirlan396@gmail.com>
2024-05-06 15:54:28 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { MyButton } from "./Button";
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
