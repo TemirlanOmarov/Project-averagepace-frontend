5a8a2e7f8d683a2827e79c1bf67bb26ee464fcb4
Temirlan Omarov <omarovtemirlan396@gmail.com>
2024-05-06 16:18:11 +0500

    Add GG

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { MyButton } from "./Button";
 export default function MyApp() {
   return (
     <div>
-      <h1>Counters that update separately</h1>
+      <h1>Counters that update separately. GG</h1>
       <MyButton />
       <MyButton />
     </div>
