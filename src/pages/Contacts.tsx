e7c2f8a1d384b329d3d13682887acf6e5528bc14
Temirlan Omarov <omarovtemirlan396@gmail.com>
2024-05-20 21:22:45 +0500

    add item form

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
 export const Contacts = () => {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleClick(t: string) {
+    setPassword(t);
+  }
+
+  function handleClick2(t: string) {
+    setLogin(t);
+  }
+
+  function logCredentials() {
+    localStorage.setItem(
+      'TEST',
+      JSON.stringify({
+        username: login,
+        password,
+      }),
+    );
+    console.log({
+      username: login,
+      password,
+    });
+  }
   return (
     <div>
       <p>Phone number: 444-444</p>
+      <input
+        type="text"
+        value={login}
+        placeholder="Login"
+        onChange={(e) => handleClick2(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => handleClick(e.target.value)}
+      />
+
+      <button onClick={logCredentials}>Login</button>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,59 @@
+import { useState } from 'react';
 import { RunningItem } from '../components/RunningItem';
 import { data } from '../constants/data';
 
 export const Home = () => {
+  const [items, setItems] = useState(data);
+  const [date, setDate] = useState('');
+  const [distance, setDistance] = useState('');
+  const [duration, setDuration] = useState('');
+
+  const handleClick = () => {
+    setItems((prevItems) => [
+      ...prevItems,
+      {
+        duration: +duration,
+        date,
+        distance: +distance,
+      },
+    ]);
+    setDate('');
+    setDistance('');
+    setDuration('');
+  };
+  const handleDate = ({ target }) => {
+    setDate(target.value);
+  };
+  const handleDistance = ({ target }) => {
+    setDistance(target.value);
+  };
+  const handelDuration = ({ target }) => {
+    setDuration(target.value);
+  };
+
   return (
     <div>
-      {data.map((item) => {
+      {items.map((item) => {
         return <RunningItem item={item} />;
       })}
+      <div>
+        <label htmlFor="date">Date:</label>
+        <input id="date" value={date} onChange={handleDate} />
+      </div>
+      <div>
+        <label htmlFor="distance">distance:</label>
+        <input
+          id="distance"
+          type="number"
+          value={distance}
+          onChange={handleDistance}
+        />
+      </div>
+      <div>
+        <label htmlFor="duration">duration:</label>
+        <input id="duration" value={duration} onChange={handelDuration} />
+      </div>
+      <button onClick={handleClick}>Create item</button>
     </div>
   );
 };
